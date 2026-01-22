9f0fde21334267045e42d5fe2247cf791f645a7c
NathanDReis <nathandavidreis@gmail.com>
2026-01-22 18:09:46 -0300

    feat(docker): criando docker compose

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const $html = document.querySelector("html");
+const $siteMenu = document.querySelector(".site-menu");
 
+/* Theme */
 function alterTheme() {
     const mapTheme = {
         "terr": "terr-dark",
@@ -15,3 +17,30 @@ function alterTheme() {
 
 const theme = localStorage.getItem("theme");
 $html.classList.add(theme || "light");
+
+/* Menu */
+let activeSiteMenu = innerWidth >= 1920;
+function toggleSiteMenu() {
+    activeSiteMenu = !activeSiteMenu;
+    siteMenu();
+}
+function siteMenu() {
+    if (activeSiteMenu) {
+        $siteMenu.classList.add("actived");
+    } else {
+        $siteMenu.classList.remove("actived");
+    }
+}
+siteMenu();
+
+function addNote() {
+
+} 
+
+function addFolder() {
+    
+}
+
+function sync() {
+
+}
---
+++ src/services/folder.service.js
@@ -0,0 +1,10 @@
+export class Folder {
+  constructor(name) { 
+    this.name = name;
+  }
+
+  static createFoler() {
+    if (!this.name || !this.name.trim().length) return;
+
+  }
+}
